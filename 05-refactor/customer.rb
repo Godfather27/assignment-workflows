717f20efd8b02afc4b4c96372c8f615a788b2cbc
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-12-14 13:58:19 +0100

    refactor customer + rental
    move set_renter_points to rental
    rename set_renter_points to frequent_renter_points
    renter_points belong to rental class so the function is moved to its class

--- 05-refactor/customer.rb
+++ 05-refactor/customer.rb
@@ -14,7 +14,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points = set_renter_points(frequent_renter_points, element)
+      frequent_renter_points += element.frequent_renter_points
 
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
@@ -24,14 +24,5 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
-  end
-
-  def set_renter_points(frequent_renter_points, element)
-    frequent_renter_points += 1
-    # add bonus for a two day new release rental
-    if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-      frequent_renter_points += 1
-    end
-    frequent_renter_points
   end
 end
